3ab19cdfb7de4bb6ead9161518679acd94254eea
P. Kartofl <kartofl@users.noreply.github.com>
2015-09-22 12:52:59 +0200

    Multi lanuguage models update

---
+++ egs/audioscope/s5/local/lm2words.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+if len(sys.argv) != 2:
+  print "Usage: python arpa2lex.py <arpa_file>"
+  exit(1)
+
+try:
+  arpa_file = open(sys.argv[1])
+except:
+  print "Couldn't open arpa file ", sys.argv[1]
+  exit(2)
+
+for line in arpa_file:
+  if line == "\\1-grams:\n":
+    break
+
+print "sil"
+
+for line in arpa_file:
+  if line == '\n' or line == "\\end":
+    break
+
+  word = line.split()[1]
+
+  if word != "<s>" and word != "</s>" and word != "sil":
+    print word
+
+arpa_file.close()
